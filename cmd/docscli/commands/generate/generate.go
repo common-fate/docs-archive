7384509e49a8d78f787c0069f5983df6c7560a59
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-11-15 08:30:34 +0800

    adjust generated output

--- cmd/docscli/commands/generate/generate.go
+++ cmd/docscli/commands/generate/generate.go
@@ -3,7 +3,8 @@ package generate
 import (
 	"os"
 	"path"
-	"strconv"
+	"strings"
+	"text/template"
 
 	"github.com/common-fate/granted-approvals/accesshandler/pkg/providerregistry"
 	"github.com/common-fate/granted-approvals/accesshandler/pkg/providers"
@@ -15,6 +16,10 @@ import (
 var GenerateCommand = cli.Command{
 	Name: "generate",
 	Action: func(c *cli.Context) error {
+		err := os.RemoveAll("./docs/approvals/providers/docs/")
+		if err != nil {
+			return err
+		}
 		registry := providerregistry.Registry()
 		for providerType, providerVersions := range registry.Providers {
 			for providerVersion, registeredProvider := range providerVersions {
@@ -22,8 +27,8 @@ var GenerateCommand = cli.Command{
 					cfg := configer.Config()
 					setuper, ok := registeredProvider.Provider.(providers.SetupDocer)
 					if ok {
-						providerVersionFolder := path.Join("./docs/approvals/providers/docs/", providerType, providerVersion)
-						err := os.MkdirAll(providerVersionFolder, os.ModePerm)
+						providerFolder := path.Join("./docs/approvals/providers/", providerType)
+						err := os.MkdirAll(providerFolder, os.ModePerm)
 						if err != nil {
 							return err
 						}
@@ -33,13 +38,45 @@ var GenerateCommand = cli.Command{
 						if err != nil {
 							return err
 						}
-						for i, instruction := range instructions {
-							f, err := os.Create(path.Join(providerVersionFolder, strconv.Itoa(i)+".md"))
-							if err != nil {
-								return err
+						f, err := os.Create(path.Join(providerFolder, providerVersion+".md"))
+						if err != nil {
+							return err
+						}
+						defer f.Close()
+
+						tmpl, err := template.New("instruction").Parse(InstructionTemplate)
+						if err != nil {
+							return err
+						}
+						instructionData := TemplateData{
+							Steps:    []Step{},
+							Provider: providerType,
+							Version:  providerVersion,
+						}
+						for _, inst := range instructions {
+							step := Step{
+								Title:        inst.Title,
+								Instructions: inst.Instructions,
+								ConfigFields: []ConfigField{},
 							}
-							defer f.Close()
-							f.WriteString(instruction.Instructions)
+							for _, field := range inst.ConfigFields {
+								step.ConfigFields = append(step.ConfigFields, ConfigField{
+									Key:         field.Key(),
+									Description: field.Description(),
+								})
+							}
+							instructionData.Steps = append(instructionData.Steps, step)
+						}
+
+						instructionsOutput := new(strings.Builder)
+						tmpl.Option()
+						err = tmpl.ExecuteTemplate(instructionsOutput, "instruction", instructionData)
+						if err != nil {
+							return err
+						}
+						_, err = f.WriteString(instructionsOutput.String())
+						if err != nil {
+							return err
 						}
 					}
 				}
@@ -49,3 +86,34 @@ var GenerateCommand = cli.Command{
 		return nil
 	},
 }
+
+type ConfigField struct {
+	Key         string
+	Description string
+}
+type Step struct {
+	Title        string
+	Instructions string
+	ConfigFields []ConfigField
+}
+
+type TemplateData struct {
+	Steps    []Step
+	Provider string
+	Version  string
+}
+
+const InstructionTemplate string = `# {{ .Provider }}@{{ .Version }}
+{{- range $ix, $option := .Steps}}
+## {{ $option.Title }}
+### Configuration Fields
+This step will guide you through collecting the values for these fields required to setup your provider.
+
+| Field | Description |
+| ----------- | ----------- |
+{{- range $ix, $field := $option.ConfigFields}}
+| {{ $field.Key }} | {{ $field.Description }} |
+{{- end}}
+{{ $option.Instructions }}
+{{- end}}
+`
